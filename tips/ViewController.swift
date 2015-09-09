e73cb1daf1d0175ee1edf1f5f1394d67771a76ee
James Tang <james@walmartlabs.com>
2015-09-09 01:18:40 -0700

    .

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        billField.text = ""
+        
+        var userDefaults = NSUserDefaults.standardUserDefaults()
+
+        if (userDefaults.objectForKey("LastBillAmount") != nil) {
+            billField.text = userDefaults.stringForKey("LastBillAmount")
+        }
+
+
+        if (userDefaults.objectForKey("LastSegmentIndex") != nil) {
+            tipControl.selectedSegmentIndex = userDefaults.integerForKey("LastSegmentIndex")
+        }
+        
+        var tipPercentages = [0.18, 0.20, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text   = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.20, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
 
+        tipLabel.text   = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+        
+        var userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setDouble(billAmount, forKey: "LastBillAmount")
+        userDefaults.setInteger(tipControl.selectedSegmentIndex, forKey: "LastSegmentIndex")
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
